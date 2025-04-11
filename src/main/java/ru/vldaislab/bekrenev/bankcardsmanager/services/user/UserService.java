2e4178e44e3361155635c8b419a06535498c78d7
Vladislav <vladislav.bekreneff@yandex.ru>
2025-04-11 22:38:18 +0300

    изменен репозиторий транзакций

--- src/main/java/ru/vldaislab/bekrenev/bankcardsmanager/services/user/UserService.java
+++ src/main/java/ru/vldaislab/bekrenev/bankcardsmanager/services/user/UserService.java
@@ -3,6 +3,7 @@ package ru.vldaislab.bekrenev.bankcardsmanager.services.user;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.vldaislab.bekrenev.bankcardsmanager.entity.user.User;
 import ru.vldaislab.bekrenev.bankcardsmanager.repositories.CardRepository;
 import ru.vldaislab.bekrenev.bankcardsmanager.repositories.TransactionRepository;
 import ru.vldaislab.bekrenev.bankcardsmanager.repositories.UserRepository;
@@ -17,6 +18,8 @@ public class UserService {
     private final TransactionRepository transactionRepository;
     private final CardService cardService;
 
-
+    public void createUser(User user) {
+        userRepository.save(user);
+    }
 
 }
